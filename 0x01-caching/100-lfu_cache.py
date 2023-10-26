6768bec8ad4c06e3533fecba9f49322700cd8564
KishMiracle <miraclekish@gmail.com>
2023-10-26 05:46:13 +0100

    100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -2,23 +2,36 @@
 from base_caching import BaseCaching
 
 
-class MRUCache(BaseCaching):
+class LFUCache(BaseCaching):
     def __init__(self):
         super().__init__()
-        self.usage_order = []  # Track the usage order of keys
+        self.frequency_counter = {}  # Track the frequency of each key
 
     def put(self, key, item):
         if key is None or item is None:
             return
 
         if key in self.cache_data:
-            # Key already exists, move it to the front of the usage order (MRU)
-            self.usage_order.remove(key)
-        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            # Cache is full, discard the MRU item
-            mru_key = self.usage_order.pop(0)
-            del self.cache_data[mru_key]
-            print(f"DISCARD: {mru_key}\n")
+            # Key already exists, increase its frequency
+            self.frequency_counter[key] += 1
+        else:
+            # Add new key with frequency 1
+            self.frequency_counter[key] = 1
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # Cache is full, discard the LFU item
+            lfu_keys = [k for k, v in self.frequency_counter.items() if v == min(self.frequency_counter.values())]
+            if len(lfu_keys) > 1:
+                # If there are multiple LFU keys, use LRU algorithm to decide which to discard
+                lru_key = self.usage_order.pop(0)
+                del self.cache_data[lru_key]
+                self.frequency_counter.pop(lru_key)
+                print(f"DISCARD: {lru_key} (LFU tiebreaker)\n")
+            else:
+                lfu_key = lfu_keys[0]
+                del self.cache_data[lfu_key]
+                self.frequency_counter.pop(lfu_key)
+                print(f"DISCARD: {lfu_key} (LFU)\n")
 
         self.cache_data[key] = item
         self.usage_order.append(key)
@@ -27,7 +40,8 @@ class MRUCache(BaseCaching):
         if key is None or key not in self.cache_data:
             return None
 
-        # Move the accessed key to the front of the usage order (MRU)
+        # Increase frequency and move key to correct position in usage_order
+        self.frequency_counter[key] += 1
         self.usage_order.remove(key)
         self.usage_order.append(key)
 
